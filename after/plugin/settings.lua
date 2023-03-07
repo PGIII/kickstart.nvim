10eb4c8aa7f944ffda51b790a00c161b9aef4d35
Preston Garrison III <pg3@flightone.com>
2023-03-07 12:01:47 -0500

    Add custom settings

---
+++ after/plugin/settings.lua
@@ -0,0 +1,8 @@
+vim.opt.updatetime = 50
+vim.opt.termguicolors = true
+vim.opt.incsearch = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
