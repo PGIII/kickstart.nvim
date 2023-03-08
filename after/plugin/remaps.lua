aa8435fd8a84548f2b934d5b0a11e0138fc9b84d
Preston Garrison III <pg3@flightone.com>
2023-03-08 00:41:49 -0500

    Add format remap

--- after/plugin/remaps.lua
+++ after/plugin/remaps.lua
@@ -3,10 +3,12 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>fb", vim.cmd.Ex)
 -- Im told this command is evil
 vim.keymap.set("n", "Q", "<nop>")
--- chmod +x 
+-- chmod +x
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 -- undo tree
 vim.keymap.set("n", "<leader>u", ":UndotreeShow<CR>")
 -- move when in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- Format files
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
